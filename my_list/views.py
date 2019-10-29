dc285fbc3174988a9525c5789596d1274954aee2
perfect igbadumhe <graphicdojo442@gmail.com>
2019-10-29 17:37:23 +0000

    Front end and requirement files

--- my_list/views.py
+++ my_list/views.py
@@ -1,5 +1,15 @@
+import requests
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+
 
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    context = {
+        'search':search
+    }
+    return render(request, 'my_list/new_search.html', context)

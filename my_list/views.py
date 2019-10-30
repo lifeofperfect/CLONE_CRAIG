a652e62127ca2c662558299f172addf51035171d
perfect igbadumhe <graphicdojo442@gmail.com>
2019-10-30 10:58:57 +0000

    scraping with beautiful soup without images yet

--- my_list/views.py
+++ my_list/views.py
@@ -1,15 +1,41 @@
 import requests
+from requests.compat import quote_plus
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+from . import models
 
-
+BASE_URL = 'http://losangeles.craigslist.org/search/?query={}'
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
 
 def new_search(request):
     search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    final_url = BASE_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+
+    post_listings = soup.find_all('li', {'class':'result-row'})
+    #post_title = post_listings[0].find(class_='result-title').text
+    #post_url = post_listings[0].find('a').get('href')
+    #post_price = post_listings[0].find(class_='result-price')
+
+    final_postings = []
+
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        final_postings.append((post_title, post_url, post_price))
+
     context = {
-        'search':search
+        'search':search,
+        'final_postings':final_postings,
     }
     return render(request, 'my_list/new_search.html', context)
